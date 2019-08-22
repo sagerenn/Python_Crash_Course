14e12cb4b866dbd0ebbeb8d39a3b738a59072164
Sage Ren <sage@jpole.onmicrosoft.com>
2019-08-22 23:14:45 +0800

    variable

---
+++ Chapter02/hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# The upper case variable is different from the lower case.
+# The interpret language will run until the error line, conparing with the compile language, that will check the systax in the process of compile.
+
+
+message = 'Hello World! var'
+print(message)
+Message2 = "Hello World! var2"
+message2 = "Hello World! var3"
+
+print(Message2,message,sep="#",end="YYY\n")
+print(message3)
+
---
+++ Chapter02/simple_messages.py
@@ -0,0 +1,6 @@
+a="hello"
+print(a)
+b=a
+print(b)
+a="Hi"
+print(a,b)
