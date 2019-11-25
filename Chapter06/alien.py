2fea94863250c41f80e9d353f5d9e591ec264470
Sage Ren <sage@jpole.onmicrosoft.com>
2019-11-25 21:16:29 +0800

    complete the book

--- Chapter06/alien.py
+++ Chapter06/alien.py
@@ -30,3 +30,96 @@ alient_0['color'] = 'red'
 alient_0['points'] = 15
 print(alient_0)
 
+alient_0['color'] = 'green'
+print(alient_0)
+
+
+food = {"meat":"fish"}
+print(food['meat'])
+food['meat'] = "crab"
+print(food['meat'])
+
+alient_0 = {
+        'color': 'green',
+        'x_position': 0,
+        'y_position': 25,
+        'speed': 'medium'
+}
+
+print(alient_0)
+
+if alient_0['speed'] == 'slow':
+        x_increment = 1
+elif alient_0['speed'] == 'medium':
+        x_increment = 2
+elif alient_0['speed'] == 'high':
+        x_increment = 3
+
+alient_0['x_position'] += x_increment
+
+print(alient_0,type(alient_0['x_position']))
+
+del(alient_0['color'])
+print(alient_0)
+
+favorite_languages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python',
+}
+
+print(favorite_languages)
+
+favorite_languages = {
+        'jen': 'python',
+        'sarah': 'c',
+        'edward': 'ruby',
+        'phil': 'python'
+        }
+
+print(favorite_languages)
+
+print(f"I love {favorite_languages['phil'].title()}")
+
+print(favorite_languages.get('susan','haha'))
+print(favorite_languages.get('susan'))
+
+if favorite_languages.get('susan') is None:
+        print("chacha")
+
+print(favorite_languages.get('phil'))
+
+
+person = {
+        "first_name": "Hedy",
+        "last_name": "He",
+        "age": "24",
+        "city": "guangzhou",
+}
+
+print(person)
+
+
+favorite_numbers = {
+        "wangsu": 6,
+        "wangsu2": 7,
+        "wangsu3": 8,
+        "wangsu4": 9,
+        "wangsu5": 10,
+}
+
+for key, value in favorite_numbers.items():
+        print(f"{key}: {value}")
+
+print(f"dfgdfgdfg {favorite_numbers.get('wangsu10','sdferg')}")
+
+programming_words = {
+        "list": "the infomation stored in a object",
+        "string": "a set of character",
+        "condition": "if...elif..else",
+        "dictionary": "associated array",
+}
+
+for key,value in programming_words.items():
+        print(f"{key}\n\t{value}\n")
